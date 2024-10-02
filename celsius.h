2ff8f46a8033abf4e3b9a501701c509635240dd0
pam <pam@gmail.com>
2024-10-02 15:37:43 -0400

    Adjusting the while loop to the do while loop

--- celsius.h
+++ celsius.h
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define CONVERSION_FACTOR 32.0
 
--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(int argc , char* argv[])
 	if(strcmp( argv[1] , psswrd) == 0 ){
 		system("clear");
 		printf("\n");
-		system("speedtest");
+		//system("speedtest");
 		menu();	
 	}else{
 		system("clear");
